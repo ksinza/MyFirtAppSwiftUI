91bef15b66294f254f73fd1c561dbf917c3c2d27
kevin sinza <sbsco3@SBSCO3s-MacBook-Pro.local>
2022-07-17 17:08:18 -0500

    working UX the first screen about sign up and sing in

--- GameStream/Shared/ContentView.swift
+++ GameStream/Shared/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            
+            Spacer()
             Color( red: 19/255, green: 30/255, blue: 53/255, opacity: 1).ignoresSafeArea()
             VStack{
                 Image("appLogo").resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 250)
+                    .padding( .bottom, 42 )
                 
                InicioYRegistroView()
             }
@@ -27,20 +28,71 @@ struct ContentView: View {
 
 struct InicioYRegistroView:View {
     
+    @State var tipoInicioSession: Bool   = true
+    
+    
     var body: some View {
         
         VStack{
             
             HStack {
-                Text("INICIA SESSION")
-                Text("REGISTRATE")
+                
+                Spacer()
+                
+                Button ("INICIA SESSION") {
+                    print("iniciar session")
+                    tipoInicioSession=true;
+                }
+                .foregroundColor(tipoInicioSession ? .white : .gray)
+                
+                
+                Spacer()
+                
+                Button ("REGISTRATE") {
+                    print("REGISTRATE")
+                    tipoInicioSession=false;
 
+                }
+                
+                .foregroundColor(tipoInicioSession ? .gray : .white)
+                
+                Spacer()
+              
             }
             
+            Spacer(minLength: 42)
+            
+            if tipoInicioSession == true{
+                InicioSessionView()
+                
+            }else{
+                
+                RegistroView()
+                
+            }
         }
         
     }
 }
+
+
+struct  InicioSessionView: View {
+    
+    var body: some View {
+        
+        Text("Soy la vista de session view")
+    }
+}
+
+
+struct  RegistroView: View {
+    
+    var body: some View {
+        
+        Text("Soy la vista de RegistroView")
+    }
+}
+
 
 
 struct ContentView_Previews: PreviewProvider {
