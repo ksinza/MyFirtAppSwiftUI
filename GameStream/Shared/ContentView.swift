672b1e4c5ccf77eb762aef6b559e73ce709c9aeb
kevin sinza <sbsco3@SBSCO3s-MacBook-Pro.local>
2022-07-20 12:01:50 -0500

    add second screen

--- GameStream/Shared/ContentView.swift
+++ GameStream/Shared/ContentView.swift
@@ -104,6 +104,7 @@ struct  InicioSessionView: View {
                     
                     
                     TextField("", text: $correo)
+                        .foregroundColor(.white)
                     
                     
                     
@@ -132,7 +133,8 @@ struct  InicioSessionView: View {
                     
                     
                     SecureField("", text: $passwords)
-                    
+                        .foregroundColor(.white)
+
                     
                     
                     
@@ -224,10 +226,201 @@ struct  InicioSessionView: View {
 
 
 struct  RegistroView: View {
+    @State var correo = ""
     
+    @State var passwords = ""
+    @State var Confirmepasswords = ""
     var body: some View {
         
-        Text("Soy la vista de RegistroView")
+        ScrollView {
+            
+            VStack(alignment: .center){
+                
+                Text("Elije una foto de perfil")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                Text("Puedes cambiar o legirla mas adelante")
+                    .font(.footnote)
+                    .fontWeight(.light)
+                    .foregroundColor(.gray)
+                    .padding(.bottom,10)
+                
+                Button(action: tomarFoto, label: {
+                    
+                    
+                    ZStack{
+                        Image("perfilejemplo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                            
+                        Image(systemName:"camera")
+                            .foregroundColor(.white)
+                        
+                        
+                    }
+                    
+                })
+                .padding(.bottom,10)
+                
+            
+            }
+                
+  
+
+            VStack (alignment: .leading) {
+
+                
+                VStack (alignment: .leading){
+                    Text("Correo Electronico*")
+                        .foregroundColor(Color("Dark-Cian"))
+                        .frame(width: 300,  alignment: .leading)
+
+                ZStack(alignment: .leading){
+
+                    if correo.isEmpty{
+
+                        Text("ejemplo@gmail.com")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+
+
+
+                    TextField("", text: $correo)
+                        .foregroundColor(.white)
+
+
+
+
+                }
+
+
+                Divider().frame(height: 1)
+                    .background(Color("Dark-Cian"))
+                    .padding(.bottom)
+
+                Text("Password*")
+                    .foregroundColor(.white)
+
+                ZStack(alignment: .leading){
+
+                    if passwords.isEmpty{
+
+                        Text("escribe tu password")
+                            .font(.caption)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+                    }
+
+
+
+                    SecureField("", text: $passwords)
+                        .foregroundColor(.white)
+
+
+
+
+                }
+
+
+                Divider().frame(height: 1)
+                    .background(Color("Dark-Cian"))
+                    .padding(.bottom)
+
+                Text("Confirmar Password*")
+                    .foregroundColor(.white)
+
+                ZStack(alignment: .leading){
+
+                    if Confirmepasswords.isEmpty{
+
+                        Text("Vuelve a escribir tu password")
+                            .font(.caption)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+                    }
+
+
+
+                    SecureField("", text: $Confirmepasswords)
+                        .foregroundColor(.white)
+
+
+
+
+                }
+
+
+                Divider().frame(height: 1)
+                    .background(Color("Dark-Cian"))
+                    .padding(.bottom)
+                
+                
+              
+            }
+
+                Button (action: registrate,label: {
+                    Text("REGISTRATE")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity,
+                               alignment: .center)
+                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                        .overlay(RoundedRectangle(cornerRadius: 6.0)
+                            .stroke(Color("Dark-Cian"),
+                                    lineWidth: 3.0
+                                   ).shadow(color: .white,
+                                            radius: 6))
+
+
+                    //.frame(width: .infinity, height: 100, alignment: .center)
+                })
+
+                Text("Registrate con redes sociales")
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity,
+                           alignment: .center)
+                    .padding(EdgeInsets(top: 31, leading: 18, bottom: 11, trailing: 18))
+                HStack {
+
+
+                    Button(action: iniciarSessionFacebook,label:{
+
+                        Text("Facebook").foregroundColor(.white)
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity,
+                                   alignment: .center)
+                            .padding(.vertical, 3.0)
+                            .background(Color("Blue-Gray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+
+                    })
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("blue-gray")/*@END_MENU_TOKEN@*/)
+
+                    Spacer()
+
+                    Button(action: iniciarSessionTwitter,label:{
+
+                        Text("Twitter").foregroundColor(.white)
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity,
+                                   alignment: .center)
+                            .padding(.vertical, 3.0)
+                            .background(Color("Blue-Gray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                    })
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("blue-gray")/*@END_MENU_TOKEN@*/)
+
+
+
+                }
+
+            }.padding(.horizontal, 77.0)
+            
+        }
+        
     }
 }
 
@@ -237,7 +430,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         
         
-        Image("pantalla01").resizable()
+        Image("pantalla02").resizable()
         
         ContentView()
         
@@ -252,6 +445,11 @@ func iniciarSession(){
     print("Estoy Iniciando sesion")
 }
 
+func registrate(){
+    
+    print("Estoy registratandome")
+}
+
 func iniciarSessionTwitter(){
     
     print("Estoy Iniciando sesion Twitter")
@@ -260,6 +458,13 @@ func iniciarSessionTwitter(){
 func iniciarSessionFacebook(){
     
     print("Estoy Iniciando sesion Facebook")
+}
+
+func tomarFoto(){
+    
+    print("Voy a tomar foto")
+
+    
 }
 
 
