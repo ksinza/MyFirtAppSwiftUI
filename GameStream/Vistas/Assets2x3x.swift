c9e14ed7046999d2b2abd3f878a1b4d52124cb5a
kevin sinza <sbsco3@SBSCO3s-MacBook-Pro.local>
2022-08-20 19:47:15 -0500

    views dummy

---
+++ GameStream/Vistas/Assets2x3x.swift
@@ -0,0 +1,23 @@
+//
+//  Assets2x3x.swift
+//  GameStream (iOS)
+//
+//  Created by SBSCO3 on 20/08/22.
+//
+
+import SwiftUI
+
+struct Assets2x3x: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Assets2x3x_Previews: PreviewProvider {
+    static var previews: some View {
+        Assets2x3x()
+        
+      //  Assets2x3x("iPhone 11")
+
+    }
+}
---
+++ GameStream/Vistas/SistemaPuntos.swift
@@ -0,0 +1,30 @@
+//
+//  SistemaPuntos.swift
+//  GameStream (iOS)
+//
+//  Created by SBSCO3 on 20/08/22.
+//
+
+import SwiftUI
+
+struct SistemaPuntos: View {
+    var body: some View {
+        
+        
+        
+        Rectangle().frame(width: 100, height: 100, alignment: .center)
+            .foregroundColor(.blue)
+        
+        
+    }
+}
+
+struct SistemaPuntos_Previews: PreviewProvider {
+    static var previews: some View {
+        SistemaPuntos()
+        
+       // SistemaPuntos()
+
+        
+    }
+}
